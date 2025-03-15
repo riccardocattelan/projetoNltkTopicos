db1cdca8a96f331582c9826051639d3a27f216c8
riccardocattelan <riccardo.cattelan0@gmail.com>
2025-03-14 23:01:53 -0300

    codigo mais limpo e funcionando com os exemplos. com limitacoes

--- projetoNltkTopicos.py
+++ projetoNltkTopicos.py
@@ -1,7 +1,6 @@
-# BACKUP
+####FUNCIONA OK
 
-##################################################################################################
-
+#####################################################
 
 import nltk
 from nltk.tokenize import word_tokenize
@@ -12,8 +11,10 @@ from math import sqrt
 import spacy
 nlp=spacy.load("pt_core_news_lg")
 
+# PARA VER UM CONTEXTO MELHOR, PODE DESCOMENTAR OS PRINT
 
-#criar uma lista com dentro as sentencas
+
+#nesse def vai criar uma lista grande com dentro mini listas com uma sentenca cada.
 def listaTokenizada(texto):
   stop_words = set(stopwords.words('portuguese'))
   listaComSentencasSeparadas=[]
@@ -29,12 +30,12 @@ def listaTokenizada(texto):
   # print('Essa é a lista com todas as sentencas dentro separadas em uma lista interna cada uma:', listaComSentencasSeparadas)  #agora tem uma lista maior com varias listas dentro, cada uma das mini listas tem uma sentenca (em teoria)
   return listaComSentencasSeparadas
 
-#unir sentenças
+#aqui vai fazer a formula da similaridade dos cossenos
 def formulaCosseno(listaComSentencasSeparadas):
   listaDasSimilaridades=[]
   for k in range(len(listaComSentencasSeparadas)-1):
     uniaoDuasSentencas=set(listaComSentencasSeparadas[k]+listaComSentencasSeparadas[k+1])
-    uniaoDuasSentencas=list(uniaoDuasSentencas)
+    uniaoDuasSentencas=list(uniaoDuasSentencas)  #aqui uniu duas sentencas para ser feita a comparacao
     # print()
     # print('Essa é a uniao de 2 sentencas, sem repeticao, para ser usada na comparacao e na formula: ', uniaoDuasSentencas) # aqui vai ter uma sentenca unica que na vdd é a uniao de duas sentencas, que nem "a dog and a cat + a cat and a frog = a dog and cat frog"
     # print('Primeira sentenca a ser comparada:', listaComSentencasSeparadas[k])
@@ -48,14 +49,16 @@ def formulaCosseno(listaComSentencasSeparadas):
       for j in range(len(listaComSentencasSeparadas[k])):
         if uniaoDuasSentencas[i]==listaComSentencasSeparadas[k][j]:
           cont+=1
-      aparecimentoPalavrasSentenca1.append(cont)
+      aparecimentoPalavrasSentenca1.append(cont) #nessa variavel tem uma lista de numeros, cada numero representa se apareceu a palavra referente a mesma posicao comparada
+      #a uniao das duas frases em comparacao, sem palavras repetidas. Para visualizar pode printar ou ver no slide da prof, em um slide onde ela fala da formula dos cossenos,
+      #q ela usa o exemplo a cat and a frog, la tem uma lista de numeros, a ideia daquela lista é essa daqui. Essa lista serve so para calcular a similaridade
 
     for i in range(len(uniaoDuasSentencas)):
       cont=0
       for j in range(len(listaComSentencasSeparadas[k+1])):
         if uniaoDuasSentencas[i]==listaComSentencasSeparadas[k+1][j]:
           cont+=1
-      aparecimentoPalavrasSentenca2.append(cont)
+      aparecimentoPalavrasSentenca2.append(cont) #aqui tb é aquela lista com aqueles numeros q tem um exemplo no slide
     # print('Quantidade de vezes que aparace cada palavra na primeira sentenca, com base a uniao das sentencas: ', aparecimentoPalavrasSentenca1)
     # print('Quantidade de vezes que aparace cada palavra na segunda sentenca, com base a uniao das sentencas: ', aparecimentoPalavrasSentenca2)
 
@@ -93,20 +96,24 @@ def mediaSentencas(listaDasSimilaridades):
   mediaSimilaridades=somaDasSimilaridades/quantidadeDeSimilaridades
   return mediaSimilaridades
 
-
+#nesse def vai criar uma lista, com mini listas dentro. Cada mini lista vai ter algumas palavras que sao as entidades de cada bloco de sentenca (bloco de sentenca seria
+#as sentencas juntas quando similares ou sozinhas se nao similares, cada bloco vai ter uma lista de palavras de entidades), as entidades sao tipo nomes, lugares, eventos etc.,
+#basicamente palavras chaves que podem ser usadas como palavras nos topicos, elas sao achadas como o spacy
 def spacysentencas(listaDasSimilaridades):
   listaEntidades=[]
-  for i in range(len(listaDasSimilaridades)):
+  for i in range(len(listaDasSimilaridades)): #vai executar o negocio a quantidade de blocos, entao tipo em um texto de 4 sentencas, e a sentenca 1 ta junta com sentenca 2
+    #por causa de similaridade ja vai ser um bloco, e a sentenca 3 e 4 tao sozinhas pq tem similaridade com nd, entao sao 3 blocos (sent1+sent2, sent3, sent4), entao o for ai
+    #vai ser feito 3 vezes, para achar as entidades de 3 sentencas, nesse exemplo
     contador=0
     contadorUnidas=0
-    if listaDasSimilaridades[i]!=0:
+    if listaDasSimilaridades[i]!=0: #aqui se a similaridade for diferente q 0, significa que tem similaridade, logo sao pelo menos 2 sentencas juntas
       frase=nltk.tokenize.sent_tokenize(texto)
       sentenca1=nlp(str(frase[i+contadorUnidas]))
-      sentenca2=nlp(str(frase[i+1+contadorUnidas]))
+      sentenca2=nlp(str(frase[i+1+contadorUnidas])) #o i+1 é pq a sentenca i é uma sentenca, e quero pegar logo a proxima, entao i+1
       # print("\nprimeira frase para buscar as entidades com spacy ", sentenca1)
       # print("segunda frase para buscar as entidades com spacy ", sentenca2)
-      listaEntidades1=[]
-      listaEntidades2=[]
+      listaEntidades1=[] #vai receber as sentencas da sentenca 1
+      listaEntidades2=[] #aqui da segunda
       listaPalavrasTopicos1=[]
       for entidadesDaSentenca1 in sentenca1.ents:
         # print(f"a frase - {sentenca1} - tem entidade: " , entidadesDaSentenca1, "|", entidadesDaSentenca1.label_)
@@ -121,15 +128,16 @@ def spacysentencas(listaDasSimilaridades):
       # print("reconhecimento de entidades frase 1", listaEntidades1)
       # print("reconhecimento de entidades frase 2", listaEntidades2)
       # print(listaPalavrasTopicos1)
-      for entidade1 in listaEntidades1:
-        for entidade2 in listaEntidades2:
-          if entidade1==entidade2:
-            contador+=1
+      # for entidade1 in listaEntidades1:
+      #   for entidade2 in listaEntidades2:
+      #     if entidade1==entidade2:
+      #       contador+=1 #aqui é so um contador de similaridades das entidades, n serve pra nd, pode ignorar
       # print("contador similaridades de entidades, ver quantas vezes tem o mesmo tipo de entidades em ambas as frases", contador)
     contadorUnidas+=1
-    if listaDasSimilaridades[i]==0:
+    if listaDasSimilaridades[i]==0: #aqui a similaridade de uma sentenca é 0, entao ela vai ser um bloco de uma unica sentenca
         frase=nltk.tokenize.sent_tokenize(texto)
-        sentenca1=nlp(str(frase[i+contadorUnidas]))
+        sentenca1=nlp(str(frase[i+contadorUnidas])) #o frase[i] é pq quero pegar a sentenca q ta indicada pela ordem do primeiro for la no topo+contadorUnidas pq se caso
+        #ja teve uma comparacao anterior, vai ser pego a proxima sentenca, senao pega a mesma sentenca que foi pega antes
         # print("\nprimeira frase para buscar as entidades com spacy ", sentenca1)
         listaEntidades1=[]
         listaPalavrasTopicos1=[]
@@ -141,9 +149,10 @@ def spacysentencas(listaDasSimilaridades):
         # print("reconhecimento de entidades frase 1", listaEntidades1)
         # print(listaPalavrasTopicos1)
         # print("LISTA PALAVRAS TOPICOS 1", listaPalavrasTopicos1)
-  print(listaEntidades)
+  # print(listaEntidades) #essa lista é a mesma coisa da primeira lista de sentencas, com mini listas, so que ai é com as entidades
   return listaEntidades
 
+#aqui vai separar em blocos e colocar os topicos logo abaixo
 def separarSentencasEtopicos(mediaSimilaridades, listaDasSimilaridades, listaEntidades):
   #separar as sentencas
   stop_words = set(stopwords.words('portuguese'))
@@ -152,44 +161,42 @@ def separarSentencasEtopicos(mediaSimilaridades, listaDasSimilaridades, listaEnt
   contadorUnicas=0
   contadorUnidas=0
   for i in range(len(listaDasSimilaridades)):
-    if listaDasSimilaridades[i]>=mediaSimilaridades:
+    if listaDasSimilaridades[i]>=mediaSimilaridades: #se a similaridade for acima da media das similaridades entao vai pegar as similaridades de duas sentencas no minimo
       listaTopicos=[]
       contadorUnidas=0
-      sentencasSimilaresUnidas="\n" + sentencasMaiusculas[i]+"\n" + sentencasMaiusculas[i+1] + "\n"
+      sentencasSimilaresUnidas="\n" + sentencasMaiusculas[i]+"\n" + sentencasMaiusculas[i+1] + "\n" #aqu vai pegar 2 sentencas e uni-las como antes, refiz o processo pq n sabia
+      #como unir com o processo ja feito antes
       print("SENTENCAS COM SIMILARIDADE \n", sentencasSimilaresUnidas)
       sentencasSimilaresUnidas=word_tokenize(sentencasSimilaresUnidas.lower())
       for palavrasUnidas in sentencasSimilaresUnidas:
         if palavrasUnidas.isalnum() and palavrasUnidas not in stop_words:
           listaPalavrasLimpas.append(palavrasUnidas)
       frequencia=FreqDist(listaPalavrasLimpas)
-      frequenciaTopicos=frequencia.most_common(5)
-      contadorUnidas+=1
-      # print("DREQUENCIA TOPICOS UNIDAS", frequenciaTopicos) ####
+      frequenciaTopicos=frequencia.most_common(5) #aqui sao as palavras mais frequentes da sentencas, pode ser usadas nos topicos, tem a palavra e quantas vezes apareceu
   # print(frequenciaTopicos)
       for topico in frequenciaTopicos:
         if topico[1]>=2:
-          listaTopicos.append(topico[0])
+          listaTopicos.append(topico[0]) #aqui vai na lista de palavras frequentes de antes e pega somente a palavra, sem a quantidade de repeticoes. Ai se caso uma palavra
+          #apareceu mais de 2 vezes, bota na lista de topicos
     # print("palavra/palavras com mais de uma aparicao em ambas as frases que estao sendo comparadas", listaTopicos)
     # print(listaPalavrasTopicos1)
-      if len(listaTopicos)<5:
+      if len(listaTopicos)<5: #tem q ter 5 palavras topicos, entao se tem menos de 5 palavras ainda, vai colocar as entidades pra completar 5 palavras
         contador=0
-        # print("AQUI OH", listaEntidades[contadorUnidas+contadorUnicas][contador]) ####
-        while len(listaTopicos) < 5 and contador < len(listaTopicos):
-          listaTopicos.append(listaEntidades[contadorUnidas+contadorUnicas][contador])
-          contador+=1
-        # print(listaTopicos) ####
-      if len(listaTopicos)<5:
+        if len(listaEntidades[contadorUnidas + contadorUnicas]) > 0:
+          while len(listaTopicos) < 5 and contador < len(listaEntidades[contadorUnidas + contadorUnicas]):
+            listaTopicos.append(listaEntidades[contadorUnidas+contadorUnicas][contador])
+            contador+=1
+      if len(listaTopicos)<5: #se caso ainda n completou, vai pegar o resto de palavras da sentenca e vai inserir nos topicos
         for j in range(len(frequenciaTopicos)):
           if frequenciaTopicos[j][0] not in listaTopicos and len(listaTopicos)<5:
             listaTopicos.append(frequenciaTopicos[j][0])
+      contadorUnidas+=1
       print("OS 5 TOPICOS \n", listaTopicos, "\n")
-    ##################################
-    if listaDasSimilaridades[i]<mediaSimilaridades:
+    if listaDasSimilaridades[i]<mediaSimilaridades: #aqui é a mesma coisa de antes, so que quando a similaridade for abaixo da media, o q muda de antes acho que é so no
+      #primeiro if, talvez da pra unir em um so processo
       listaPalavrasLimpas=[]
       listaTopicos=[]
       sentencasSimilaridade=sentencasMaiusculas[i+contadorUnidas]
-      # print(contadorUnidas) ####
-      # print(sentencasSimilaridade) ####
       print("SENTENCAS SEM SIMILARIDADE \n", sentencasSimilaridade, "\n")
       # contadorUnicas+=1
       sentencasSimilaridade=word_tokenize(sentencasSimilaridade.lower())
@@ -198,7 +205,6 @@ def separarSentencasEtopicos(mediaSimilaridades, listaDasSimilaridades, listaEnt
           listaPalavrasLimpas.append(palavrasUnidas)
       frequencia=FreqDist(listaPalavrasLimpas)
       frequenciaTopicos=frequencia.most_common(5)
-      # print("FREQUENCIA TOPICOS UNICAS", frequenciaTopicos) ####
       for topico in frequenciaTopicos:
         if topico[1]>=2:
           listaTopicos.append(topico[0])
@@ -206,15 +212,10 @@ def separarSentencasEtopicos(mediaSimilaridades, listaDasSimilaridades, listaEnt
       # print(listaPalavrasTopicos1)
       if len(listaTopicos)<5:
         contador=0
-        # print("AQUI OH ", listaEntidades[contadorUnidas + contadorUnicas][contador]) ####
-        # print(contadorUnidas + contadorUnicas, contador) ####
-        # while len(listaTopicos)<5 and contador <= len(listaTopicos):  ####
-        # print("OUT OF RANGE", listaEntidades[contadorUnicas+contadorUnidas][contador]) ####
-        while len(listaTopicos) < 5 and contador < len(listaEntidades[contadorUnidas + contadorUnicas]):
-          listaTopicos.append(listaEntidades[contadorUnidas + contadorUnicas][contador])
-          contador += 1
-          # print(listaTopicos) ####
-          # print("LISTA TOPICOS UNICAS", listaTopicos) ####
+        if len(listaEntidades[contadorUnidas + contadorUnicas]) > 0:
+          while len(listaTopicos) < 5 and contador < len(listaEntidades[contadorUnidas + contadorUnicas]):
+            listaTopicos.append(listaEntidades[contadorUnidas + contadorUnicas][contador])
+            contador += 1
       if len(listaTopicos)<5:
         for j in range(len(frequenciaTopicos)):
           if frequenciaTopicos[j][0] not in listaTopicos and len(listaTopicos)<5:
@@ -232,11 +233,15 @@ media=mediaSentencas(cosseno)
 spcy=spacysentencas(cosseno)
 separar=separarSentencasEtopicos(media, cosseno, spcy)
 print()
-print('Todos os resultados que foram gerados por meio da formula das somatorias. Depois é a média entre elas): ', cosseno, media)
+# print('Todos os resultados que foram gerados por meio da formula das somatorias. Depois é a média entre elas): ', cosseno, media)
 
 #falta:
 #botar os numeros ate 2 casas decimais
-#continuar o codigo, separando sentencas e etc
 #ver se a separacao em def ta ok
 #perguntar se pode def sem return
 #ver as aspas para python mais antigos
+
+#limitacoes:
+#o codigo ta meio limitado em 2 sentencas em cada bloco onde sentencas tem similaridades, se caso tiver um bloco com 3 sentencas unidas similares ou mais, acho q n funciona
+#as palavras nos topicos, quando tem poucas palavras repetidas mais de 2 vezes e poucas entidades, acabam entrando as primeiras palavras da sentenca, mesmo n sendo
+#palavras importantes
